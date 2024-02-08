492c7cae5fcd96de818f9983872197cc58121ba7
neil <jacoborosseau@gmail.com>
2024-02-08 01:16:09 -0500

    flex-column

--- src/home/pages/NostrosPage.tsx
+++ src/home/pages/NostrosPage.tsx
@@ -5,12 +5,12 @@ import { Button } from "@/components/ui/button";
 export const NostrosPage = () => {
   return (
     <HomeLayout>
-      <div className="flex justify-around items-center h-screen bg-gradient-to-r from-[#279FD2]  to-[#FFFFFF]">
-        <div className="flex justify-around w-2/5">
-            <Button className="bg-azulmarino text-2xl rounded-xl" >AGTECH</Button>
-            <Button className="bg-azulmarino text-2xl rounded-xl">INDUSTRYTECH</Button>
+        <div className="flex justify-around items-center h-screen bg-gradient-to-r from-[#279FD2]  to-[#FFFFFF]">
+            <div className="flex justify-around w-2/5">
+                <Button className="bg-azulmarino text-2xl rounded-xl" >AGTECH</Button>
+                <Button className="bg-azulmarino text-2xl rounded-xl">INDUSTRYTECH</Button>
+            </div>
         </div>
-      </div>
     </HomeLayout>
   );
 };
