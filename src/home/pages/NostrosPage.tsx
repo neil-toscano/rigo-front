2f77b3389ec0d75c444c3f9e8d9649a40ea19fac
rafaelneyen1234 <132718963+rafaelneyen1234@users.noreply.github.com>
2024-02-08 01:10:39 -0500

    flexbox

--- src/home/pages/NostrosPage.tsx
+++ src/home/pages/NostrosPage.tsx
@@ -6,9 +6,15 @@ export const NostrosPage = () => {
   return (
     <HomeLayout>
       <div className="flex justify-around items-center h-screen bg-gradient-to-r from-[#279FD2]  to-[#FFFFFF]">
-        <div className="flex justify-around w-2/5">
-            <Button className="bg-azulmarino text-2xl rounded-xl" >AGTECH</Button>
-            <Button className="bg-azulmarino text-2xl rounded-xl">INDUSTRYTECH</Button>
+        <div className="flex flex-col space-y-20 w-5/6 content-around">
+          <div className="flex justify-around w-2/5">
+              <Button className="bg-azulmarino text-2xl rounded-xl" >AGTECH</Button>
+              <Button className="bg-azulmarino text-2xl rounded-xl">INDUSTRYTECH</Button>
+          </div>
+
+          <div className="flex justify-around w-2/5">
+              <Button className="bg-greyclear text-2xl rounded-xl">SABER MÁS</Button>
+          </div>
         </div>
       </div>
     </HomeLayout>
